31df94551f821e1f135a9955236aed4cfda8aa5d
Mistakx <ade9@live.com.pt>
2021-06-24 18:16:26 +0100

    Added chat function

---
+++ node.js_chat/server.js
@@ -0,0 +1,41 @@
+let app  = require('express')();
+let http = require('http').Server(app);
+let io   = require('socket.io')(http);
+
+
+
+let people = {};
+
+
+
+// Server start
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+
+
+// Client connects to the server
+io.on('connection', function(socket){
+
+    // Client joins the server
+    socket.on("join", function(clientUsername){
+        //console.log(clientUsername+" has joined the server.");
+        people[socket.id] = clientUsername;
+        io.emit("update", clientUsername + " has joined the server.");
+    });
+
+    // Client sends chat message
+    socket.on('chatMessage', function(clientMessage){
+        //console.log('message: ' + clientMessage);
+        let message = {msg:clientMessage, id:people[socket.id]};
+        io.emit('chatMessage', message);
+    })
+});
+
+
+
+// Client connects to the server route
+app.get('/', function(request, response){
+    response.render('chat.ejs');
+});
