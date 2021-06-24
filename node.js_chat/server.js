2f6be520ce349e7689144cf5b84f8621314f9378
Mistakx <ade9@live.com.pt>
2021-06-24 18:55:03 +0100

    Added connection to database

--- node.js_chat/server.js
+++ node.js_chat/server.js
@@ -2,15 +2,21 @@ let app  = require('express')();
 let ejs = require('ejs');
 let http = require('http').Server(app);
 let io   = require('socket.io')(http);
-
+let passport = require("passport");
+let mongoose = require('mongoose');
 
 
 app.set('view engine', 'ejs');
-
+mongoose.connect('mongodb+srv://G9:rtLjPDj0sF0lE5HQ@clusterdbw.1dbjr.mongodb.net/G9?authSource=admin&replicaSet=atlas-bek8xj-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 let people = {};
 
+
+// Connect to Database
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection to database error.'));
+db.once('open', function() { console.log('Connected to database.'); } );
 
 
 // Server start
@@ -41,6 +47,31 @@ io.on('connection', function(socket){
 
 
 // Client connects to the server route
-app.get('/', function(request, response){
-    response.render('chat.ejs');
+
+// app.get('/', function(request, response) {
+//
+//     response.render('chat.ejs');
+//
+// });
+
+app.get("/", (request, response) => {
+
+    const isAuthenticated = !!request.user;
+
+    // if (isAuthenticated) {
+    //     console.log(`user is authenticated, session is ${req.session.id}`);
+    // } else {
+    //     console.log("unknown user");
+    // }
+
+    response.render(isAuthenticated ? "index.ejs" : 'login.ejs');
+
+    //response.sendFile(isAuthenticated ? "index.html" : "login.html", { root: __dirname });
 });
+
+// Login post request
+app.post("/login", passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/",
+    })
+);
